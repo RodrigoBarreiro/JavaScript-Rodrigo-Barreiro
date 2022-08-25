904c7f0f0f65a02548d2cad44ad0ae1820ab6542
RodrigoBarreiro <barreiro.rb@gmail.com>
2022-08-25 16:38:50 -0300

    cambie numero maximo de turnos

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // prompt con ciclo FOR da turnos hasta 10 turnos por dias 
 
-for (let i= 1; i <= 4; i++) {
+for (let i= 1; i <= 5; i++) {
     let ingresarNombre = prompt("ingresar Nombre");
     let ingreseApellido = prompt("Ingrese Apellido");
     alert("Turno N°"+i+" para: "+ingresarNombre + " " + ingreseApellido);
 } 
 
-alert ("no quedan mas turnos por el dia de hoy");
+alert ("no quedan mas turnos por el dia de hoy"); 
 
 
 // prompt con ciclo While ingresa presion arterial mientras la presion sea anormal la sigue pidiendo sale del ciclo cuando da presion normal
