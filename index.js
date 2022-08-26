2cb4950af4629a879fdd0a2a358c6a7557d7fca2
RodrigoBarreiro <barreiro.rb@gmail.com>
2022-08-25 23:22:12 -0300

    arregle valores de presion

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ for (let i= 1; i <= 5; i++) {
     alert("Turno N°"+i+" para: "+ingresarNombre + " " + ingreseApellido);
 } 
 
-alert ("no quedan mas turnos por el dia de hoy"); 
+alert ("no quedan mas turnos por el dia de hoy");
 
 
 // prompt con ciclo While ingresa presion arterial mientras la presion sea anormal la sigue pidiendo sale del ciclo cuando da presion normal
 
 let numero = parseFloat  (prompt ("ingrese su presion arterial"));
 
-while ( numero <10 || numero > 13) {
+while ( numero <9.6 || numero > 14.8) {
     alert ("Presion arterial fuera de lo normal.");
     numero = parseFloat  (prompt ("ingrese su presion arterial"));
 }
